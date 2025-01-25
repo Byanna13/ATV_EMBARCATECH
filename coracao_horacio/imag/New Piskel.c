07a5f55616d46746d2314635fc8a7d8accae2bbe
Byanna13 <fabianasouza012@gmail.com>
2025-01-25 08:56:06 -0300

    imagem e matriz

---
+++ coracao_horacio/imag/New Piskel.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#define NEW_PISKEL_FRAME_COUNT 1
+#define NEW_PISKEL_FRAME_WIDTH 5
+#define NEW_PISKEL_FRAME_HEIGHT 5
+
+/* Piskel data for "New Piskel" */
+
+static const uint32_t new_piskel_data[1][25] = {
+{
+0x00000000, 0xffbf0e67, 0x00000000, 0xffbf0e67, 0x00000000, 
+0xffbf0e67, 0x00000000, 0xffbf0e67, 0x00000000, 0xffbf0e67, 
+0xffbf0e67, 0x00000000, 0x00000000, 0x00000000, 0xffbf0e67, 
+0x00000000, 0xffbf0e67, 0x00000000, 0xffbf0e67, 0x00000000, 
+0x00000000, 0x00000000, 0xffbf0e67, 0x00000000, 0x00000000
+}
+};
